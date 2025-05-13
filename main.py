4b264eb349bf71645ecdd878db613d9f1107a4bc
BT_HOME_DESKTOP\borat <bora.tarhan@gmail.com>
2025-05-12 22:40:42 -0500

    added packages

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import sys
 
 def main():
     print("Hello from btraderoption!")
+
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
     print(sys.executable)
 
 if __name__ == "__main__":
