ced77ed97336ee6453886c1645c95e95461a82eb
BT_HOME_DESKTOP\borat <bora.tarhan@gmail.com>
2025-05-12 22:46:21 -0500

    cleaned up

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import sys
 def main():
     print("Hello from btraderoption!")
 
-    print("123")
-    
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
     print(sys.executable)
 
 if __name__ == "__main__":
